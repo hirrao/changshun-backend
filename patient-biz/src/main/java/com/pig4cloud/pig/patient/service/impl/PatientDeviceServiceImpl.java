5599c1ae0ee66cd4f298671f2c991637cacf78d0
wangwenche <wangwenche1@outlook.com>
2024-07-05 16:41:57 +0800

    患者设备相关信息管理功能完成，增删改查

---
+++ patient-biz/src/main/java/com/pig4cloud/pig/patient/service/impl/PatientDeviceServiceImpl.java
@@ -0,0 +1,16 @@
+package com.pig4cloud.pig.patient.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.pig4cloud.pig.patient.entity.PatientDeviceEntity;
+import com.pig4cloud.pig.patient.mapper.PatientDeviceMapper;
+import com.pig4cloud.pig.patient.service.PatientDeviceService;
+import org.springframework.stereotype.Service;
+/**
+ * 
+ *
+ * @author wangwenche
+ * @date 2024-07-04 20:47:58
+ */
+@Service
+public class PatientDeviceServiceImpl extends ServiceImpl<PatientDeviceMapper, PatientDeviceEntity> implements PatientDeviceService {
+}
---
+++ patient-biz/src/main/java/com/pig4cloud/pig/pig/service/PatientDeviceService.java
@@ -0,0 +1,8 @@
+package com.pig4cloud.pig.pig.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.pig4cloud.pig.pig.entity.PatientDeviceEntity;
+
+public interface PatientDeviceService extends IService<PatientDeviceEntity> {
+
+}
