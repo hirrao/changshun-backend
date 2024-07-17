57c2605ea200729b9fc0596ec7e760eb09efadec
Curry30huang <1832647481@qq.com>
2024-07-17 18:28:02 +0800

    更新医患绑定服务

--- patient-biz/src/main/java/com/pig4cloud/pig/patient/service/PatientDoctorService.java
+++ patient-biz/src/main/java/com/pig4cloud/pig/patient/service/PatientDoctorService.java
@@ -1,11 +1,17 @@
 package com.pig4cloud.pig.patient.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.pig4cloud.pig.common.core.util.R;
 import com.pig4cloud.pig.patient.dto.HeartRateStatsDTO;
 import com.pig4cloud.pig.patient.entity.PatientDoctorEntity;
 
 public interface PatientDoctorService extends IService<PatientDoctorEntity> {
-    HeartRateStatsDTO getHeartRateStats(Long doctorUid);
-
-    //boolean setPatientDoctorBinding(Long patientUid, Long doctorUid);
+	
+	HeartRateStatsDTO getHeartRateStats(Long doctorUid);
+	
+	//boolean setPatientDoctorBinding(Long patientUid, Long doctorUid);
+	
+	R addItem(PatientDoctorEntity patientDoctor);
+	
+	R updateItem(PatientDoctorEntity patientDoctor);
 }
--- patient-biz/src/main/java/com/pig4cloud/pig/patient/service/impl/PatientDoctorServiceImpl.java
+++ patient-biz/src/main/java/com/pig4cloud/pig/patient/service/impl/PatientDoctorServiceImpl.java
@@ -2,13 +2,14 @@ package com.pig4cloud.pig.patient.service.impl;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.pig4cloud.pig.common.core.util.R;
 import com.pig4cloud.pig.patient.dto.HeartRateStatsDTO;
-import com.pig4cloud.pig.patient.entity.PatientBmiManaEntity;
 import com.pig4cloud.pig.patient.entity.PatientDoctorEntity;
 import com.pig4cloud.pig.patient.mapper.PatientDoctorMapper;
 import com.pig4cloud.pig.patient.service.PatientDoctorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 /**
  * 医患绑定表
  *
@@ -16,16 +17,47 @@ import org.springframework.stereotype.Service;
  * @date 2024-07-08 23:56:30
  */
 @Service
-public class PatientDoctorServiceImpl extends ServiceImpl<PatientDoctorMapper, PatientDoctorEntity> implements PatientDoctorService {
-    private final PatientDoctorMapper patientDoctorMapper;
-
-    @Autowired
-    public PatientDoctorServiceImpl(PatientDoctorMapper patientDoctorMapper) {
-        this.patientDoctorMapper = patientDoctorMapper;
-    }
-
-    @Override
-    public HeartRateStatsDTO getHeartRateStats(Long doctorUid) {
-        return patientDoctorMapper.getHeartRateStats(doctorUid);
-    }
+public class PatientDoctorServiceImpl extends
+ ServiceImpl<PatientDoctorMapper, PatientDoctorEntity> implements PatientDoctorService {
+	
+	private final PatientDoctorMapper patientDoctorMapper;
+	
+	@Autowired
+	public PatientDoctorServiceImpl(PatientDoctorMapper patientDoctorMapper) {
+		this.patientDoctorMapper = patientDoctorMapper;
+	}
+	
+	@Override
+	public HeartRateStatsDTO getHeartRateStats(Long doctorUid) {
+		return patientDoctorMapper.getHeartRateStats(doctorUid);
+	}
+	
+	@Override
+	public R addItem(PatientDoctorEntity patientDoctor) {
+		// 检查医生和患者是否已经绑定
+		LambdaQueryWrapper<PatientDoctorEntity> wrapper = new LambdaQueryWrapper<>();
+		wrapper.eq(PatientDoctorEntity::getDoctorUid, patientDoctor.getDoctorUid());
+		wrapper.eq(PatientDoctorEntity::getPatientUid, patientDoctor.getPatientUid());
+		PatientDoctorEntity one = this.getOne(wrapper);
+		if (one != null) {
+			return R.failed("医生和患者已经绑定");
+		}
+		this.save(patientDoctor);
+		return R.ok("绑定成功");
+	}
+	
+	@Override
+	public R updateItem(PatientDoctorEntity patientDoctor) {
+		// 根据医患绑定关系更新特别关心
+		LambdaQueryWrapper<PatientDoctorEntity> wrapper = new LambdaQueryWrapper<>();
+		wrapper.eq(PatientDoctorEntity::getDoctorUid, patientDoctor.getDoctorUid());
+		wrapper.eq(PatientDoctorEntity::getPatientUid, patientDoctor.getPatientUid());
+		PatientDoctorEntity one = this.getOne(wrapper);
+		if (one != null) {
+			one.setCare(patientDoctor.getCare());
+			this.updateById(one);
+			return R.ok("更新成功");
+		}
+		return R.failed("医生和患者已经绑定");
+	}
 }
