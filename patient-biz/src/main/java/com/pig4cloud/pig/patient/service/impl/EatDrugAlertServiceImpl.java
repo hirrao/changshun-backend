027ea575a5da1bd388d6b5250aa189d0a5ae0786
Curry30huang <1832647481@qq.com>
2024-07-30 23:00:33 +0800

    修改用药提醒信息

--- patient-biz/src/main/java/com/pig4cloud/pig/patient/service/impl/EatDrugAlertServiceImpl.java
+++ patient-biz/src/main/java/com/pig4cloud/pig/patient/service/impl/EatDrugAlertServiceImpl.java
@@ -1,7 +1,9 @@
 package com.pig4cloud.pig.patient.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.pig4cloud.pig.patient.entity.DrugEatTimeEntity;
 import com.pig4cloud.pig.patient.entity.EatDrugAlertEntity;
 import com.pig4cloud.pig.patient.entity.SysMessageEntity;
 import com.pig4cloud.pig.patient.mapper.EatDrugAlertMapper;
@@ -9,15 +11,16 @@ import com.pig4cloud.pig.patient.mapper.SysMessageMapper;
 import com.pig4cloud.pig.patient.service.DrugEatTimeService;
 import com.pig4cloud.pig.patient.service.EatDrugAlertService;
 import com.pig4cloud.pig.patient.service.WebsocketService;
+import java.sql.Time;
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.List;
 import lombok.extern.slf4j.Slf4j;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
-
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.List;
 
 /**
  * 用药管理表
@@ -27,45 +30,86 @@ import java.util.List;
  */
 @Service
 @Slf4j
-public class EatDrugAlertServiceImpl extends ServiceImpl<EatDrugAlertMapper, EatDrugAlertEntity> implements EatDrugAlertService {
-
-    @Autowired
-    private EatDrugAlertMapper eatDrugAlertMapper;
-
-    @Autowired
-    private WebsocketService websocketService;
-
-    @Autowired
-    private SysMessageMapper sysMessageMapper;
-
-    @Override
-    public List<EatDrugAlertEntity> getActiveAlerts() {
-        return eatDrugAlertMapper.selectList(new QueryWrapper<EatDrugAlertEntity>().eq("is_active", 1));
-    }
-
-    @Scheduled(cron = "0 0 * * * ?") // 每小时检查一次
-    @Override
-    public void sendDrugAlerts() {
-        List<EatDrugAlertEntity> alerts = getActiveAlerts();
-        for (EatDrugAlertEntity alert : alerts) {
-            String message = String.format("请记得服用药物: %s，剂量: %d%s",
-                    alert.getDrugName(), alert.getDose(), alert.getUnit());
-            websocketService.sendMsg(alert.getPatientUid(), message);
-
-            // 添加系统消息
-            SysMessageEntity sysMessage = new SysMessageEntity();
-            sysMessage.setDoctorUid(null);
-            sysMessage.setPatientUid(alert.getPatientUid());
-            sysMessage.setMessageType("用药提醒");
-            sysMessage.setJsonText("{\"drugName\":\"" + alert.getDrugName() + "\",\"dose\":\"" + alert.getDose() + "\",\"unit\":\"" + alert.getUnit() + "\"}");
-            sysMessage.setSentDate(LocalDateTime.now());
-            sysMessage.setIsRead(false);
-            addSysMessage(sysMessage);
-        }
-    }
-    @Override
-    public void addSysMessage(SysMessageEntity sysMessage) {
-        sysMessageMapper.insert(sysMessage);
-    }
-
+public class EatDrugAlertServiceImpl extends
+ ServiceImpl<EatDrugAlertMapper, EatDrugAlertEntity> implements EatDrugAlertService {
+	
+	@Autowired
+	private EatDrugAlertMapper eatDrugAlertMapper;
+	
+	@Autowired
+	private DrugEatTimeService drugEatTimeService;
+	
+	@Autowired
+	private WebsocketService websocketService;
+	
+	@Autowired
+	private SysMessageMapper sysMessageMapper;
+	
+	@Override
+	public List<EatDrugAlertEntity> getActiveAlerts() {
+		return eatDrugAlertMapper.selectList(
+		 new QueryWrapper<EatDrugAlertEntity>().eq("is_active", 1));
+	}
+	
+	@Scheduled(cron = "0 0/30 * * * ?") // 每半个小时检查一次
+	@Override
+	public void sendDrugAlerts() {
+		List<EatDrugAlertEntity> alerts = getActiveAlerts();
+		for (EatDrugAlertEntity alert : alerts) {
+			// 根据计划寻找用药提醒时间
+			LambdaQueryWrapper<DrugEatTimeEntity> wrapper = new LambdaQueryWrapper<>();
+			DrugEatTimeEntity tmp = new DrugEatTimeEntity();
+			tmp.setPdeId(alert.getPdeId());
+			wrapper.setEntity(tmp);
+			List<DrugEatTimeEntity> list = drugEatTimeService.list(wrapper);
+			LocalDateTime now = LocalDateTime.now();
+			// 遍历列表
+			for (DrugEatTimeEntity item : list) {
+				// 检测当天是否已经用药过
+				Duration durationToday = Duration.between(now, item.getLastEatTime());
+				if (durationToday.toDays() <= 0) {
+					// 无需提醒
+					continue;
+				}
+				// 检测用药时间和当前时间是否相差一小时
+				LocalTime nowTime = LocalTime.now();
+				Duration nowBetween = Duration.between(nowTime, item.getEatTime().toLocalTime());
+				if (nowBetween.toHours() <= 1) {
+					// 提醒
+					String message = String.format("请记得服用药物: %s，剂量: %d%s，时间:%s",
+					 alert.getDrugName(), alert.getDose(), alert.getUnit(), item.getEatTime());
+					websocketService.sendMsg(alert.getPatientUid(), message);
+					
+					// 添加系统消息
+					SysMessageEntity sysMessage = getSysMessageEntity(alert, now,
+					 item.getEatTime());
+					addSysMessage(sysMessage);
+				}
+				
+			}
+			
+			
+		}
+	}
+	
+	private static @NotNull SysMessageEntity getSysMessageEntity(EatDrugAlertEntity alert,
+	 LocalDateTime now, Time alertTime) {
+		SysMessageEntity sysMessage = new SysMessageEntity();
+		sysMessage.setDoctorUid(null);
+		sysMessage.setPatientUid(alert.getPatientUid());
+		sysMessage.setMessageType("用药提醒");
+		sysMessage.setJsonText(
+		 "{\"drugName\":\"" + alert.getDrugName() + "\",\"dose\":\"" + alert.getDose()
+		  + "\",\"unit\":\"" + alert.getUnit() + "\"," + "\"eatTime:\":" + "\""
+		  + alertTime.toString() + "\"" + "}");
+		sysMessage.setSentDate(now);
+		sysMessage.setIsRead(false);
+		return sysMessage;
+	}
+	
+	@Override
+	public void addSysMessage(SysMessageEntity sysMessage) {
+		sysMessageMapper.insert(sysMessage);
+	}
+	
 }
