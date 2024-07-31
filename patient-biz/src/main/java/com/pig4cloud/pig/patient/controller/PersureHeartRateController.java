0b5ec5ebb7b233d5c6ce481e052e17888d538a1e
wangwenche <wangwenche1@outlook.com>
2024-07-31 16:13:28 +0800

    二维码生成

--- patient-biz/src/main/java/com/pig4cloud/pig/patient/controller/PersureHeartRateController.java
+++ patient-biz/src/main/java/com/pig4cloud/pig/patient/controller/PersureHeartRateController.java
@@ -23,7 +23,6 @@ import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
