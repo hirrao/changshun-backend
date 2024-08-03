66b6434dbdb41988790422ffd97135ad37324c01
Curry30huang <1832647481@qq.com>
2024-08-03 13:13:05 +0800

    更新医生生成二维码，通过nginx做静态资源管理

--- doctor-biz/src/main/java/com/pig4cloud/pig/doctor/controller/DoctorQcodeController.java
+++ doctor-biz/src/main/java/com/pig4cloud/pig/doctor/controller/DoctorQcodeController.java
@@ -5,20 +5,16 @@ import com.pig4cloud.pig.common.core.util.R;
 import com.pig4cloud.pig.common.security.annotation.Inner;
 import com.pig4cloud.pig.doctor.utils.QRCodeUtil;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import java.io.File;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springdoc.core.converters.FileSupportConverter;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpHeaders;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.File;
 
 /**
  * <h3>doctor</h3>
@@ -34,31 +30,32 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(description = "QRcode", name = "医生Id二维码管理")
 @Slf4j
 public class DoctorQcodeController {
-	
-	@Value("${web.qrcode}")
-	private String qrcodePath;
-	
-	
-	@Inner(value = false)
-	@Operation(summary = "生成医生二维码", description = "生成医生二维码")
-	@PostMapping("/generate")
-	public R generate(@RequestBody JSONObject jsonObject) {
-		// 获取医生Uid
-		Long doctorUid = jsonObject.getLong("doctorUid");
-		// 验证静态资源是否存在
-		String path = qrcodePath + '/' + doctorUid + ".png";
-		File file = new File(path);
-		if (file.exists()) {
-			return R.ok(path);
-		} 
-		// 不存再生成
-		try {
-			QRCodeUtil.encode(doctorUid.toString(),qrcodePath,doctorUid);
-			return R.ok(path);
-		} catch (Exception e) {
-			log.error("生成二维码错误",e);
-			return R.failed("生成二维码错误");
-		}
-		
-	}
+
+    @Value("${web.qrcode}")
+    private String qrcodePath;
+
+
+    @Inner(value = false)
+    @Operation(summary = "生成医生二维码", description = "生成医生二维码")
+    @PostMapping("/generate")
+    public R generate(@RequestBody JSONObject jsonObject) {
+        // 获取医生Uid
+        Long doctorUid = jsonObject.getLong("doctorUid");
+        // 验证静态资源是否存在
+        String path = qrcodePath + '/' + doctorUid + ".jpg";
+        String returnPath = "http://114.116.237.85:80/static/qrcode" + doctorUid + ".jpg";
+        File file = new File(path);
+        if (file.exists()) {
+            return R.ok(returnPath);
+        }
+        // 不存再生成
+        try {
+            QRCodeUtil.encode(doctorUid.toString(), qrcodePath, doctorUid);
+            return R.ok(returnPath);
+        } catch (Exception e) {
+            log.error("生成二维码错误", e);
+            return R.failed("生成二维码错误");
+        }
+
+    }
 }
