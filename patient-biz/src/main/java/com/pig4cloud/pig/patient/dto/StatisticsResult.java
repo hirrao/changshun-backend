d6fd39c6ffbbc4b57a5931b24df6e09164ba2e7c
huang-qing123456789 <1736023616@qq.com>
2024-08-06 00:19:53 +0800

    ai预问诊统计图客观病史修改

---
+++ patient-biz/src/main/java/com/pig4cloud/pig/patient/dto/StatisticsResult.java
@@ -0,0 +1,14 @@
+package com.pig4cloud.pig.patient.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class StatisticsResult {
+    private int earlyCvdFamilyHistoryCount; // 家族病史
+    private int smokingCount;                // 吸烟人数
+    private int drinkingCount;               // 饮酒人数
+    private int infectiousDiseaseCount;      // 感染病史人数
+    private int foodAllergyCount;            // 食物过敏人数
+}
