36cc3452631aea25b8b0bcd19b3dca56c45d9f8e
wangwenche <wangwenche1@outlook.com>
2024-08-06 20:32:19 +0800

    修改bug2

--- patient-biz/src/main/java/com/pig4cloud/pig/patient/controller/HeartRateLogsController.java
+++ patient-biz/src/main/java/com/pig4cloud/pig/patient/controller/HeartRateLogsController.java
@@ -168,7 +168,7 @@ public class HeartRateLogsController {
 
     @Operation(summary = "查询某一天的某位患者的心率的平均值", description = "查询某一天的某位患者的心率的平均值")
     @GetMapping("/daily_avg_heart_rate")
-    @PreAuthorize("@pms.hasPermission('patient_persureHeartRate_view')")
+    @PreAuthorize("@pms.hasPermission('patient_heartRateLogs_view')")
     public R getDailyAverageHeartRate(@RequestParam LocalDate date, @RequestParam Long patientUid) {
         JSONObject result = heartRateLogsService.getDailyAverageHeartRate(date, patientUid);
         return R.ok(result);
@@ -176,21 +176,21 @@ public class HeartRateLogsController {
 
     @Operation(summary = "查询某一周的某位患者的心率的平均值", description = "查询某一周的某位患者的心率的平均值")
     @GetMapping("/weekly_avg_heart_rate")
-    @PreAuthorize("@pms.hasPermission('patient_persureHeartRate_view')")
+    @PreAuthorize("@pms.hasPermission('patient_heartRateLogs_view')")
     public R getWeeklyAverageHeartRateByDay(@RequestParam int weeksAgo, @RequestParam Long patientUid) {
         return R.ok(heartRateLogsService.getWeeklyAverageHeartRateByDay(weeksAgo, patientUid));
     }
 
     @Operation(summary = "查询某一个月的某位患者的每周段的心率的平均值", description = "查询某一个月的某位患者的每周段的心率的平均值")
     @GetMapping("/monthly_avg_heart_rate_by_week")
-    @PreAuthorize("@pms.hasPermission('patient_persureHeartRate_view')")
+    @PreAuthorize("@pms.hasPermission('patient_heartRateLogs_view')")
     public R getMonthlyAverageHeartRateByWeek(@RequestParam int monthsAgo, @RequestParam Long patientUid) {
         return R.ok(heartRateLogsService.getMonthlyAverageHeartRateByWeek(monthsAgo, patientUid));
     }
 
     @Operation(summary = "查询某一年的每个月的心率的平均值", description = "查询某一年的每个月的心率的平均值")
     @GetMapping("/yearly_avg_heart_rate_by_month")
-    @PreAuthorize("@pms.hasPermission('patient_persureHeartRate_view')")
+    @PreAuthorize("@pms.hasPermission('patient_heartRateLogs_view')")
     public R getYearlyAverageHeartRateByMonth(@RequestParam int yearsAgo, @RequestParam Long patientUid) {
         return R.ok(heartRateLogsService.getYearlyAverageHeartRateByMonth(yearsAgo, patientUid));
     }
