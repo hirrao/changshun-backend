2146f908375e613b78cbc7f0f014c697454859d5
huang-qing123456789 <1736023616@qq.com>
2024-08-09 16:12:56 +0800

    修改返回格式

--- patient-biz/src/main/java/com/pig4cloud/pig/patient/dto/StatisticsResult.java
+++ patient-biz/src/main/java/com/pig4cloud/pig/patient/dto/StatisticsResult.java
@@ -6,7 +6,19 @@ import lombok.Data;
 @Data
 @AllArgsConstructor
 public class StatisticsResult {
-    private int earlyCvdFamilyHistoryCount;
+
+    private int 高血压家族遗传史;
+    private int 吸烟史;
+    private int 饮酒史;
+    private int 传染病史;
+    private int 食物过敏史;
+    private double 高血压家族遗传史比例;
+    private double 吸烟史比例;
+    private double 饮酒史比例;
+    private double 传染病史比例;
+    private double 食物过敏史比例;
+
+    /*private int earlyCvdFamilyHistoryCount;
     private double earlyCvdFamilyHistoryPercentage;
 
     private int smokingCount;
@@ -19,7 +31,7 @@ public class StatisticsResult {
     private double infectiousDiseasePercentage;
 
     private int foodAllergyCount;
-    private double foodAllergyPercentage;
+    private double foodAllergyPercentage;*/
     /*private int earlyCvdFamilyHistoryCount; // 家族病史
     private int smokingCount;                // 吸烟人数
     private int drinkingCount;               // 饮酒人数
