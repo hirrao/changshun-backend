2a6c88b9f053cf92bda61d3d0fcc87061ccbd051
hirrao <git@hirrao.com>
2025-08-04 12:42:26 +0800

    修复回调消息

--- patient-biz/src/main/java/com/pig4cloud/pig/patient/request/PatientDeviceCallbackRequest/IPatientDeviceCallback.java
+++ patient-biz/src/main/java/com/pig4cloud/pig/patient/request/PatientDeviceCallbackRequest/IPatientDeviceCallback.java
@@ -1,8 +1,13 @@
 package com.pig4cloud.pig.patient.request.PatientDeviceCallbackRequest;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
 /**
  * @author wangyifei
  * 接口包含回调消息中的event_data字段
  */
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.EXTERNAL_PROPERTY, property = "event_type")
+@JsonSubTypes({@JsonSubTypes.Type(value = BloodPressureCallback.class, name = "bp_event"), @JsonSubTypes.Type(value = HeartRateCallback.class, name = "hr_event"),})
 public interface IPatientDeviceCallback {
 }
